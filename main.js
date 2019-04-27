6b4d572db93371d556bd7b6e3e9a69f11b115cdf
Veranika Barel <veranikabarel@gmail.com>
2019-04-27 17:36:46 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var mymap = L.map('mapid').fitWorld();
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+let marker = L.marker([52.237049, 21.017532]).addTo(mymap);
+marker.bindPopup("I am a pin.").openPopup();
+
+let popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
