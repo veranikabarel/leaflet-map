07202a38e53f770b8b73f12eae2e1a75a9075b2f
Veranika Barel <veranikabarel@gmail.com>
2019-04-28 23:12:29 +0200

    added draggable pins and bootstrap table

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-var mymap = L.map('mapid').fitWorld();
+let mymap = L.map('mapid', {
+    doubleClickZoom: false,
+    closePopupOnClick: false,
+}).fitWorld();
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-let marker = L.marker([52.237049, 21.017532]).addTo(mymap);
-marker.bindPopup("I am a pin.").openPopup();
-
-let popup = L.popup();
+mymap.on('click', onMapClick);
 
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+    addMarker(e.latlng);
+    addPlaces(e.latlng)
 }
 
-mymap.on('click', onMapClick);
+function addMarker(latlng) {
+    L.marker(latlng, {
+        draggable: true
+    }).addTo(mymap);
+}
+
+function addPlaces(latlng) {
+    var table = document.getElementById("list");
+    var tr = document.createElement('tr');
+    tr.appendChild(document.createTextNode(latlng));
+    table.appendChild(tr);
+}
